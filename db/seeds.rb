0de61691f61b32d3fb2d400748d1c44b98923281
Yuri Reyes <reyestomas13@gmail.com>
2016-11-07 18:01:32 -0700

    Checkpoint 30 voting complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,18 @@ topics = Topic.all
 
  # Create Posts
  50.times do
-   Post.create!(
+   post = Post.create!(
     user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
    )
+
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
+
  end
  posts = Post.all
 
