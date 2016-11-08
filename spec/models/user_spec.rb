54015c5768ea8943764e82253185bfab1d23f180
Yuri Reyes <reyestomas13@gmail.com>
2016-11-08 11:47:16 -0700

    Checkpoint 31 Favorites complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe User, type: :model do
 
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:comments) }
+  it { is_expected.to have_many(:votes) }
+  it { is_expected.to have_many(:favorites) }
 
   # Shoulda tests for name
   it { is_expected.to validate_presence_of(:name) }
@@ -32,7 +34,7 @@ RSpec.describe User, type: :model do
      end
 
      it "responds to admin?" do
-       expect(user).to respond_to(:admin?)
+       expect(user).to respond_to(:member?)
      end
 
      it "responds to member?" do
@@ -83,4 +85,19 @@ RSpec.describe User, type: :model do
       expect(user_with_invalid_email).to_not be_valid
     end
 
+    describe "#favorite_for(post)" do
+      before do
+        topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+        @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+      end
+
+      it "returns `nil` if the user has not favorited the post" do
+        expect(user.favorite_for(@post)).to be_nil
+      end
+
+      it "returns the appropriate favorite if it exists" do
+        favorite = user.favorites.where(post: @post).create
+        expect(user.favorite_for(@post)).to eq(favorite)
+      end
+    end
   end
